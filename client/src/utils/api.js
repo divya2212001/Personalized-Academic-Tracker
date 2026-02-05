e2078e1c05275f1bc16893c57070c19c5254441c
divya2212001 <divyagopisetty380@gmail.com>
2026-02-06 00:40:41 +0530

    fixed error

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://personalized-academic-tracker-2.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://personalized-academic-tracker-3.onrender.com";
 
 const api = axios.create({
   baseURL: API_URL,
